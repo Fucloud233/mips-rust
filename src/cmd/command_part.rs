85ad229e33843cc6e2fa82c28b68df72cc4bfd51
Fucloud <270928474@qq.com>
2023-08-29 21:32:49 +0800

    refactor: 简化PartInfo的获取方式 static hashmap -> 实现枚举方法info

--- src/cmd/command_part.rs
+++ src/cmd/command_part.rs
@@ -1,5 +1,3 @@
-use std::collections::HashMap;
-use lazy_static::lazy_static;
 use serde::Deserialize;
 
 #[derive(Hash, Eq, PartialEq, Debug, Deserialize)]
@@ -12,38 +10,30 @@ pub enum CommandPart {
 	IMM,
 }
 
+impl CommandPart {
+    // 获取info
+    pub fn info(&self) -> &CommandPartInfo {
+        match self {
+            CommandPart::OP => &CommandPartInfo { length: 6, offset: 27},
+            CommandPart::RS => &CommandPartInfo { length: 5, offset: 22},
+            CommandPart::RT => &CommandPartInfo { length: 5, offset: 17},
+            CommandPart::RD => &CommandPartInfo { length: 6, offset: 11},
+            CommandPart::SHAMT => &CommandPartInfo { length: 5, offset: 6},
+            CommandPart::FUNCT => &CommandPartInfo { length: 6, offset: 0},
+            CommandPart::IMM => todo!(),
+        }
+        // COMMANDPARTS.get(self).unwrap()
+    }  
+    
+    // 将数字转换到对应位置上
+    pub fn convert_num(&self, num: u8) -> u32 {
+        let res: u32 = num.into();
+        res << self.info().length
+    }
+}
+
 // 指令部分中的信息
-struct CommandPartInfo {
-    length: u8,
-    offset: u8,
+pub struct CommandPartInfo {
+    pub length: u8,
+    pub offset: u8,
 }
-
-impl CommandPartInfo{
-    pub fn length(&self) -> u8 {
-        self.length
-    }
-
-    pub fn offset(&self) -> u8 {
-        self.offset
-    }
-}
-
-lazy_static! {
-    static ref CommandParts: HashMap<CommandPart, CommandPartInfo> = init_command_parts();
-}
-
-fn init_command_parts() -> HashMap<CommandPart, CommandPartInfo> {
-    let mut command_parts = HashMap::new();
-    command_parts.insert(CommandPart::OP, CommandPartInfo { length: 6, offset:  27});
-    command_parts.insert(CommandPart::RS, CommandPartInfo { length: 5, offset:  22});
-    command_parts.insert(CommandPart::RT, CommandPartInfo { length: 5, offset:  17});
-    command_parts.insert(CommandPart::RD, CommandPartInfo { length: 6, offset:  11});
-    command_parts.insert(CommandPart::SHAMT, CommandPartInfo { length: 5, offset:  6});
-    command_parts.insert(CommandPart::FUNCT, CommandPartInfo { length: 6, offset:  0});
-
-    command_parts
-}
-
-pub fn get_cmd_part_info(part_type: &CommandPart) -> Option<&CommandPartInfo> {
-    CommandParts.get(part_type)
-} 
