224ff9a2b32724adb6c89df1f215dec4e4e6cb88
Fucloud <270928474@qq.com>
2023-09-07 15:55:25 +0800

    refactor:  修改SaveFormatType定义位置

--- src/save.rs
+++ src/save.rs
@@ -1,14 +1,10 @@
 use std::{fs::File, io::Write};
+use crate::config::SaveFormatType;
 
 // use lazy_static::lazy_static;
 // lazy_static! {
 //     static ref LOGISIM_HEADER: String = String::from("v2.0 raw");
 // }
-
-#[derive(Debug, Clone)]
-pub enum SaveFormatType {
-    Logisim, Plain
-}
 
 fn save_as_logisim(codes: &Vec<u32>, file_path: &String) {
     let logisim_header = String::from("v2.0 raw");
